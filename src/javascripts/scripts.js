fbae77d8f8be15ae407331a258aaffa8668145bd
cyan1992 <maarefimorteza@gmail.com>
2024-11-05 17:40:13 +0330

    Hero Section and Nav Bar Side Section Added HTML & CSS, But Not Complate

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,10 +1,27 @@
-// Fix Nav Bar In Project when Scroll Page  
+ 
 const navbar = document.querySelector('.nav');
+const navBtn = document.querySelector("#nav_btn");
+const closeBtn = document.querySelector("#close_btn");
+const sidebar = document.querySelector("#sidebar");
 
+// Fix Nav Bar In Project when Scroll Page 
 window.addEventListener("scroll", function () {  
-  if (window.scrollY > 80) {  
+  if (window.scrollY > 0) {  
     navbar.classList.add("navbar_fixed");  
   } else {  
     navbar.classList.remove("navbar_fixed");  
   }  
 });
+
+// Show SideBar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show_sidebar");
+});
+
+// Close SideBar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show_sidebar");
+});
+
+
+
